15cde932f15c79628ca7dc65dafed21ce1f5d1e6
Najati Imam <najati@gmail.com>
2020-01-09 12:59:34 -0500

    small script to change color, hole size for floating quads

---
+++ Assets/Scripts/SetQuadColorAndHoleSize.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class SetQuadColorAndHoleSize : MonoBehaviour {
+	public Color color = Color.red;
+	public float holeRadius = 0.3f;
+
+	private void Start() {
+		holeRadius = Mathf.Clamp(holeRadius, 0, 0.5f);
+
+		var renderer = GetComponent<MeshRenderer>();
+		renderer.material.SetColor(ColorProperty, color);
+		renderer.material.SetFloat(HoleRadiusProperty, holeRadius);
+	}
+
+	private static readonly int ColorProperty = Shader.PropertyToID("_Color");
+	private static readonly int HoleRadiusProperty = Shader.PropertyToID("_HoleRadius");
+}
